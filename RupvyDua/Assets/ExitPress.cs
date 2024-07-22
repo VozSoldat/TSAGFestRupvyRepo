a15d81eebe3880cb9df3c10f2607c131cfc9012f
Muhammad Erril <133947567+VozSoldat@users.noreply.github.com>
2024-07-22 08:27:25 +0700

    add: exit button

---
+++ RupvyDua/Assets/ExitPress.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class ExitPress : MonoBehaviour
+{
+    public void startExit()
+    {
+        Application.Quit();
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
